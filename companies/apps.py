c25cc5a34301ac32009e4d810288f07309955877
AlexandreAnsiau <icealex_53@hotmail.com>
2024-09-05 22:42:20 +0200

    The compgany app is renamed company.

--- companies/apps.py
+++ companies/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class CompagniesConfig(AppConfig):
+class CompaniesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'compagnies'
+    name = 'companies'
--- companies/models.py
+++ companies/models.py
@@ -4,7 +4,7 @@ from django.utils.translation import gettext_lazy as _
 from commons.models import AbstractDatedModel, AbstractUUIDModel
 
 
-class Compagnies(AbstractUUIDModel, AbstractDatedModel):
+class Company(AbstractUUIDModel, AbstractDatedModel):
     name = models.CharField(max_length=200, verbose_name=_("nom"))
 
     def __str__(self):
