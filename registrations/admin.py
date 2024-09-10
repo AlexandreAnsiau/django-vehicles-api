8af36d3695681854b58082324eeeee21645578f9
AlexandreAnsiau <icealex_53@hotmail.com>
2024-09-10 11:51:45 +0200

    The UserCreationForm use the AUTH_PASSWORD_VALIDATORS.

--- registrations/admin.py
+++ registrations/admin.py
@@ -6,4 +6,8 @@ from .models import CustomUser
 
 @admin.register(CustomUser)
 class CustomUserAdmin(admin.ModelAdmin):
-    form = UserCreationForm
+
+    def get_form(self, request, obj=None, *args, **kwargs):
+        if not obj:
+            kwargs["form"] = UserCreationForm
+        return super().get_form(request, obj, *args, **kwargs)
--- registrations/forms.py
+++ registrations/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.auth.password_validation import validate_password
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
 
@@ -8,31 +9,37 @@ from .models import CustomUser
 class UserCreationForm(forms.ModelForm):
     """A form for creating new users. Includes all the required
     fields, plus a repeated password."""
-    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
-    password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput)
 
     class Meta:
         model = CustomUser
-        fields = ('email', 'first_name', 'last_name', 'company')
+        fields = ('email', 'first_name', 'last_name', 'company', 'password')
+        widgets = {
+            "password": forms.PasswordInput
+        }
 
-    def clean_password1(self):
-        password1 = self.cleaned_data.get("password1")
-        if len(password1) < 8:
-            raise ValidationError(_("Le mot de passe doit être composé d'au moins 8 caractères."))
-        return password1
+    password_confirmation = forms.CharField(label='Password confirmation', widget=forms.PasswordInput)
 
-    def clean_password2(self):
+    def clean_password(self):
+        password = self.cleaned_data.get("password")
+        try:
+            # AUTH_PASSWORD_VALIDATORS in the settings are used.
+            validate_password(self.cleaned_data.get("password"))
+        except ValidationError as error:
+            self.add_error('password', error)
+        return password
+
+    def clean_password_confirmation(self):
         # Check that the two password entries match
-        password1 = self.cleaned_data.get("password1")
-        password2 = self.cleaned_data.get("password2")
-        if password1 and password2 and password1 != password2:
+        password = self.cleaned_data.get("password")
+        password_confirmation = self.cleaned_data.get("password_confirmation")
+        if password and password_confirmation and password != password_confirmation:
             raise ValidationError(_("Les deux mots de passe ne sont pas identiques"))
-        return password2
+        return password_confirmation
 
     def save(self, commit=True):
         # Save the provided password in hashed format
         user = super().save(commit=False)
-        user.set_password(self.cleaned_data["password1"])
+        user.set_password(self.cleaned_data.get("password"))
         if commit:
             user.save()
         return user
