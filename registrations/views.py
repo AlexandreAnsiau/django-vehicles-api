ac68d3475954c44620458a3065fbc1576252ff11
AlexandreAnsiau <icealex_53@hotmail.com>
2024-09-12 21:55:31 +0200

    The users can change their password from the PasswordCreationView.

--- registrations/views.py
+++ registrations/views.py
@@ -1,11 +1,10 @@
 from django.contrib.auth.decorators import login_not_required
 from django.contrib.auth.views import LoginView
-from django.http import HttpResponse
-from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import FormView
 
 from .forms import PasswordCreationFrom
+from .models import ResetPasswordToken
 
 
 class CustomLoginView(LoginView):
@@ -19,6 +18,10 @@ class PasswordCreationView(FormView):
     success_url = reverse_lazy("admin:login")
 
     def form_valid(self, form):
+        token = ResetPasswordToken.objects.get(key=form.cleaned_data.get("token"))
+        token.user.set_password(form.cleaned_data.get("password"))
+        token.user.save()
+        token.delete()
         return super().form_valid(form)
 
 
